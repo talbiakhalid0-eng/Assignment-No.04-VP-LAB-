06b6fae559e4d67828dbb9644ac9d0b3133118ca
Talbia khalid <talbiakhalid0@gmail.com>
2026-05-25 20:27:05 +0500

    complete application 06 integrating custom runtime options directly inside injection pipeline workers

---
+++ Assignment No.04 VP LAB/Application 06/NotificationService.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignment_No._04_VP_LAB.Application_06
+{
+    public class NotificationService
+    {
+		private readonly NotificationConfig _globalConfiguration;
+		public NotificationService(NotificationConfig configuration) => _globalConfiguration = configuration;
+
+		public Task<List<string>> ExtractActiveSystemLogsAsync()
+		{
+			var outputContainer = new List<string>();
+			for (int i = 1; i <= _globalConfiguration.DefaultNumberOfNotifications; i++)
+			{
+				outputContainer.Add($"[Registry Frame Broadcast Engine 0{i}]: Subsystem thread node metrics mapping completely balanced.");
+			}
+			return Task.FromResult(outputContainer);
+		}
+	}
+}
